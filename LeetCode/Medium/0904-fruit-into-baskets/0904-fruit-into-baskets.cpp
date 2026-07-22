4d9cfb64ef7907ebbc15e5447705e1d3890d537c
Amit Kumar Patwa <amitpatwa80040@gmail.com>
2026-07-22 10:22:21 +0530

    Time: 24 ms (91.04%), Space: 88.8 MB (11.25%) - LeetHub

--- LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-     int n=fruits.size();
-     int l=0,r=0,len=0;
-     map<int,int>mpp;
+        int n=fruits.size();
+        int l=0,r=0,len=0;
+        map<int,int>mpp;
 
-     while(r<n){
-        mpp[fruits[r]]++;
+        while(r<n){
+            mpp[fruits[r]]++;
+            
+            //now check basket exceed or not
 
-        //if sized exceed
-        if(mpp.size()>2){
-            while(mpp.size()>2){
-                mpp[fruits[l]]--;
-                if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
-                l++;
+            if(mpp.size()>2){
+                while(mpp.size()>2){
+                    mpp[fruits[l]]--;
+
+                    if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
+
+                    l++;
+                }
             }
+            len=max(r-l+1,len);
+            r++;
         }
-        
-        len=max(len,r-l+1);
-        r++;
-     }   
-    return len;
+        return len;
     }
 };
