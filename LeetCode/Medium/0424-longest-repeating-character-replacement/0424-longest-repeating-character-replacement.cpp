5b2e349c2c0bd36725dbd637c98b1da9a38e12f9
Amit Kumar Patwa <amitpatwa80040@gmail.com>
2026-07-23 11:34:00 +0530

    Time: 0 ms (100%), Space: 10.8 MB (71.41%) - LeetHub

--- LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -4,22 +4,23 @@ public:
         int n=s.size();
         int l=0,r=0,len=0,maxf=0;
         int hash[26]={0};
+
         while(r<n){
             hash[s[r]-'A']++;
             maxf=max(maxf,hash[s[r]-'A']);
 
-            //check if vaid or not 
+            //check if valid or not
 
-            while(((r-l+1)-maxf) >k ){
+            while(((r-l+1)-maxf)>k){
                 hash[s[l]-'A']--;
                 maxf=0;
                 for(int i=0;i<26;i++) maxf=max(maxf,hash[i]);
                 l++;
             }
+            //calculate max len
 
-            if(((r-l+1)-maxf) <= k) len = max(len, r-l+1);
+            len=max(len,r-l+1);
             r++;
-
         }
         return len;
     }
