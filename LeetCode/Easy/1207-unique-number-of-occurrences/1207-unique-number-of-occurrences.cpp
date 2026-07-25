88c4b74fbf96ef6c0967c42332caedecaf9ecb0e
Amit Kumar Patwa <amitpatwa80040@gmail.com>
2026-07-25 14:48:54 +0530

    Time: 0 ms (100%), Space: 11.9 MB (68.06%) - LeetHub

---
+++ LeetCode/Easy/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        unordered_set<int>st;
+        for(auto i :mp){
+            if(st.find(i.second)!=st.end()) return false;
+            else st.insert(i.second);
+        }
+        return true;
+    }
+};
