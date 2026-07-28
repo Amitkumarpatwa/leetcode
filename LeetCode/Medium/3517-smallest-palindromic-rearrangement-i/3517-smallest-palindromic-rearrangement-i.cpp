e3d61ddc4816a7c64431a7c3784efc6edf567b8c
Amit Kumar Patwa <amitpatwa80040@gmail.com>
2026-07-28 22:59:03 +0530

    Time: 73 ms (42.2%), Space: 69.3 MB (69.23%) - LeetHub

--- LeetCode/Medium/3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ LeetCode/Medium/3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        int n = s.length();
-        int half = n / 2;
-        
-        string left = s.substr(0, half);
-        
-        sort(left.begin(), left.end());
-        
-        string right = left;
-        reverse(right.begin(), right.end());
-        
-        if (n % 2 != 0) {
-            return left + s[half] + right;
-        }
-        
-        return left + right;
+        int n=s.size();
+        int half=n/2;
+
+        string left= s.substr(0,half);
+
+        sort(left.begin(),left.end());
+
+        string right=left;
+
+        reverse(right.begin(),right.end());
+
+        if(n%2!=0) return left+s[half]+right;
+
+        return left+right;
+
     }
 };
