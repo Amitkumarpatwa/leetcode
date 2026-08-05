0a20280dc20b3fcb0358cb892906dbdffaab5bd9
Amit Kumar Patwa <amitpatwa80040@gmail.com>
2026-08-05 10:11:15 +0530

    Time: 25 ms (23.93%), Space: 29.9 MB (24.22%) - LeetHub

--- LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     string removeStars(string s) {
         stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='*'){
-                if(st.size()) st.pop();
-            }
+        int n= s.size();
+        for(int i=0;i<n;i++){
+            if(st.size()!=0 && s[i]=='*') st.pop();
             else st.push(s[i]);
         }
+
         string ans;
-        while(st.size()){
-           
+        while(st.size()>0){
             ans+=st.top();
             st.pop();
         }
+
         reverse(ans.begin(),ans.end());
+
         return ans;
+
     }
 };
