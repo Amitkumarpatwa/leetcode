56d35d66a9db29c878f59823e2190c66f9e253fe
Amit Kumar Patwa <amitpatwa80040@gmail.com>
2026-08-22 13:55:53 +0530

    Time: 1 ms (43.67%), Space: 10 MB (35.6%) - LeetHub

---
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+        for(int i=0;i<s.size();i++){
+            if(isalnum(s[i])) ans+=tolower(s[i]);
+        }
+        int i=0,j=ans.size()-1;
+
+        while(i<j){
+            if(ans[i]!=ans[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
